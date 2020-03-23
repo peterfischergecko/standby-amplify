8528294c3df03a50c64014b83bbf1bffd0cd927d
Peter Fischer <pfi@gecko.de>
2020-03-23 14:42:34 +0100

    first working prototype

--- standby/src/app/app.component.ts
+++ standby/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from './API.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'standby';
+  todos: Array<any>;
+
+  constructor(private apiService: APIService) {}
+
+  async ngOnInit() {
+    this.apiService.ListTodos().then((evt) => {
+      this.todos = evt.items;
+    });
+
+    this.apiService.OnCreateTodoListener.subscribe((evt) => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.todos = [...this.todos, data];
+    });
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+        name: 'Angular',
+        description: 'testing'
+    });
+  }
 }
--- standby/src/main.ts
+++ standby/src/main.ts
@@ -1,8 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import PubSub from '@aws-amplify/pubsub';
+import API from '@aws-amplify/api';
+import awsconfig from './aws-exports';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
